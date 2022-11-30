6bb275fee91997bbf7287cf6d4964362e867cc8d
theUmeshC <umesh.c@celestialsys.com>
2022-11-30 12:33:24 +0530

    practise node with templating engines to see the data transfer between files

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import pkgEnv from 'dotenv';
+const { DotenvConfigOptions } = pkgEnv;
+const app = express();
+import pkg from 'body-parser';
+const { urlencoded } = pkg;
+const port = +process.env.PORT || 5000;
+
+console.log(port);
+const users = [];
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.render('index', {pageTitle: 'Add User'});
+});
+
+app.get('/users', (req, res) => {
+    res.render('users', {pageTitle: 'Users', users:users});
+});
+
+app.post('/add-user', (req, res) => {
+    users.push({name: req.body.username});
+    res.redirect('/users');
+});
+
+app.use((req, res) => {
+res.status(404).render('404', {pageTitle: 'not found'});
+});
+
+app.listen(port, ()=>{
+    console.log("server listening on port " + port);
+});
